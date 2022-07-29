9244857e4fc97391cb4ca5a3a5fa66c9732d19b5
Jason Frank <jasonkylefrank@gmail.com>
2022-07-28 22:39:07 -0400

    Fixed intermittent Avatar img 403 issue

--- components/avatar.js
+++ components/avatar.js
@@ -6,11 +6,14 @@ const AvatarImg = styled.img`
 `;
 
 export default function Avatar({ src, onClick }) {
-  const diceBearSeed = 'jailbreak' + Date.now;
+  const diceBearSeed = 'niceness' + Date.now;
   // Dicebear: https://avatars.dicebear.com/
   const source = src || `https://avatars.dicebear.com/api/open-peeps/:${diceBearSeed}.svg`;
 
   return (
-    <AvatarImg src={source} alt="Avatar" onClick={onClick}></AvatarImg>
+    // The referrerPolicy attribute helps solve intermittent 403 permission problems when getting the 
+    //  user's Google avatar pic. See: https://stackoverflow.com/questions/40570117/http403-forbidden-error-when-trying-to-load-img-src-with-google-profile-pic
+    //  Note: camelCase the attribute name here in React.
+    <AvatarImg src={source} alt="Avatar" onClick={onClick} referrerPolicy="no-referrer"></AvatarImg>
   );
 }
